e90c01bd68a0e0aee594134c1bdef3f16841ae6d
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-07 21:16:13 +0300

    hover color for links, add resume icon, add projects route

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,29 +1,51 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client";
 
-import logo from "../assets/logo.svg";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+
+import { logo, resume } from "../assets";
+
+const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About Me",
+    path: "/about",
+  },
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+];
+
+const getLinkColor = (pathname: string, link: string) => {
+  if (pathname === link) {
+    return "text-textPrimary";
+  }
+};
 
 const Header = () => {
+  const pathname = usePathname();
+  console.log("🚀 ~ file: Header.tsx:11 ~ Header ~ pathname:", pathname);
 
   return (
-    <header className="flex justify-between border items-center font-bold">
-      <Image src={logo} width={32}  height={32} alt="logo" />
+    <header className="flex justify-between items-center font-bold uppercase py-12">
+      <Image src={logo} width={45} height={45} alt="logo" />
       <nav>
-        <ul className="flex justify-between gap-x-10">
-          <li className="text-textPrimary">
-            <Link href={'/'}>HOME</Link>
-          </li>
-          <li>
-            <Link href={'/about'}>ABOUT ME</Link>
-          </li>
-          <li>
-            <Link href={'/projects'}>PROJECTS</Link>
-          </li>
+        <ul className="flex justify-between gap-x-9 text-xs">
+          {links.map((link, index) => (
+            <li key={index} className={getLinkColor(pathname, link.path)}>
+              <Link href={link.path}>{link.name}</Link>
+            </li>
+          ))}
         </ul>
       </nav>
-      <button>
-        DOWNLOAD CV
-      </button>
+      <div className="p-2 rounded-full bg-primary shadow-primary/50 shadow-md cursor-pointer">
+        <Image src={resume} width={29} height={29} alt="resume" />
+      </div>
     </header>
   );
 };
