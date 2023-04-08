8acedfd998dc86b977d8e78f31930763ebd60722
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-08 10:45:55 +0300

    add content for hero section

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,2 +1,4 @@
 export { default as logo } from './logo.png';
 export { default as resume } from './resume.png';
+export { default as lineDirection } from './lineDirection.png';
+export { default as graphic } from './graphic.png';
