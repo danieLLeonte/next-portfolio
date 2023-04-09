e29db225b2f1c39e0447a7a6206ad83b14c2dad6
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-09 14:25:49 +0300

    make navbar links scroll to section

---
+++ src/app/components/About.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section id="about">
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+      <div>alo</div>
+    </section>
+  );
+};
+
+export default About;
