6d94837916ddf1b6ae51463bd0bd2db8b1431000
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-09 15:37:17 +0300

    make navbar sticky, add margin bottom

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 
 import { logo, resume } from "../assets";
@@ -24,6 +24,7 @@ const getLinkColor = (pathname: string, link: string) =>
 
 const Header = () => {
   const [pathName, setPathName] = useState<string>(window.location.hash);
+  const [shadow, setShadow] = useState<boolean>(false);
 
   const handleLinkClick = (
     e: React.MouseEvent<HTMLAnchorElement>,
@@ -37,25 +38,44 @@ const Header = () => {
     setPathName(link);
   };
 
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 60) {
+        setShadow(true);
+      } else {
+        setShadow(false);
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+
+    return () => window.removeEventListener("scroll", handleShadow);
+  }, []);
+
   return (
-    <header className="flex justify-between items-center font-bold uppercase py-12 sticky top-0 z-50 border-b-2 bg-white">
-      <Image src={logo} alt="logo" width={45} height={45} />
-      <nav>
-        <ul className="flex justify-between gap-x-9 text-xs">
-          {links.map((link, index) => (
-            <li key={index} className={getLinkColor(pathName, link.path)}>
-              <a
-                onClick={(e) => handleLinkClick(e, link.path)}
-                href={link.path}
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className="p-2 rounded-full bg-primary shadow-primary/50 shadow-md cursor-pointer">
-        <Image src={resume} width={29} height={29} alt="resume" />
+    <header
+      className={`font-bold uppercase h-20 mt-4 mb-24 sticky inset-0 z-50 flex items-center backdrop-blur-lg bg-secondary/50 ${
+        shadow && "shadow-xl ease-in-out duration-300"
+      }`}
+    >
+      <div className="flex flex-1 justify-between items-center max-w-[1440px] mx-auto px-12">
+        <Image src={logo} alt="logo" width={45} height={45} />
+        <nav>
+          <ul className="flex justify-between gap-x-9 text-xs">
+            {links.map((link, index) => (
+              <li key={index} className={getLinkColor(pathName, link.path)}>
+                <a
+                  onClick={(e) => handleLinkClick(e, link.path)}
+                  href={link.path}
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className="p-2 rounded-full bg-primary shadow-primary/50 shadow-md cursor-pointer">
+          <Image src={resume} width={29} height={29} alt="resume" />
+        </div>
       </div>
     </header>
   );
