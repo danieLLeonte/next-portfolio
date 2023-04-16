a8f843bdae736ba3446bce622f40d43ede1b6209
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-16 08:52:33 +0300

    add background elements

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,6 +1,6 @@
 export { default as logo } from './logo.svg';
 export { default as resume } from './resume.png';
-export { default as lineDirection } from './lineDirection.png';
+export { default as lineDirection } from './lineDirection.svg';
 export { default as graphic } from './graphic.png';
 export { default as workBackground } from './workBackground.jpg';
 export { default as slackLogo } from './slackLogo.png';
