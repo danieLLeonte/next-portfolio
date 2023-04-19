2d898070f3df409e721b094cfc7f21807f5c793f
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-19 20:17:21 +0300

    consume data from strapi in hero section

--- src/app/components/SocialLinks.tsx
+++ src/app/components/SocialLinks.tsx
@@ -1,11 +1,24 @@
+import Link from "next/link";
 import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/ai";
 
-const SocialLinks = () => {
+interface SocialLinksProps {
+  gmail: string;
+  linkedin: string;
+  github: string;
+}
+
+const SocialLinks = ({ gmail, linkedin, github }: SocialLinksProps) => {
   return (
     <div className="flex justify-between gap-x-10">
-      <AiFillMail className="icon" size={23} />
-      <AiFillLinkedin className="icon" size={23} />
-      <AiFillGithub className="icon" size={23} />
+      <Link href={`mailto:${gmail}`}>
+        <AiFillMail className="icon" size={23} />
+      </Link>
+      <Link href={`${linkedin}`} target="_blank" rel="noopener noreferrer">
+        <AiFillLinkedin className="icon" size={23} />
+      </Link>
+      <Link href={`${github}`} target="_blank" rel="noopener noreferrer">
+        <AiFillGithub className="icon" size={23} />
+      </Link>
     </div>
   );
 };
