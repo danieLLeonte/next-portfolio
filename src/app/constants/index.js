104bd1713525be7ff6592c3eaafa1a404d31433c
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-20 08:52:28 +0300

    refactoring

---
+++ src/app/constants/index.js
@@ -0,0 +1,22 @@
+export const people = [
+  {
+    name: "John Doe",
+    position: "CEO of Tesla",
+    image: "https://picsum.photos/200",
+    testimonial:
+      "Lorem111 ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed semper nisl. Sed euismod, nisl vel aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nisl nisl eu nunc. Sed euismod, nisl vel aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nisl nisl eu nunc.",
+  },
+  {
+    name: "John Doe",
+    position: "CEO of SpaceX",
+    testimonial:
+      "Lorem222 ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed semper nisl. Sed euismod, nisl vel aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nisl nisl eu nunc. Sed euismod, nisl vel aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nisl nisl eu nunc.",
+  },
+  {
+    name: "John Doe",
+    position: "CEO of Apple",
+    image: "https://picsum.photos/200",
+    testimonial:
+      "Lorem333 ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed semper nisl. Sed euismod, nisl vel aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nisl nisl eu nunc. Sed euismod, nisl vel aliquam lacinia, nisl nisl aliquet nisl, eget aliquam nisl nisl eu nunc.",
+  },
+];
