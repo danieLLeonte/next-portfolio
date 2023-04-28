468b5e7c87348eaf56da2bed4b3b10555fc25ce9
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-28 10:20:20 +0300

    update style modal contact form

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -13,9 +13,9 @@ export const Modal = ({
 
   return (
     <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex z-50 items-center justify-center">
-      <div className="bg-white w-5/6 sm:w-3/6 md:w-3/7 min-[1030px]:w-2/6 min-[1500px]:w-2/7 h-1/2 rounded-lg shadow-primary/30 shadow-md">
+      <div className="w-72 sm:w-96 rounded-2xl shadow-xl shadow-primary/20 flex-col justify-between bg-white">
         <div className="flex justify-end">
-          <HiX className="w-7 h-7 m-2 text-black" onClick={onClose} />
+          <HiX className="w-7 h-7 m-2 text-black opacity-80" onClick={onClose} />
         </div>
         {children}
       </div>
