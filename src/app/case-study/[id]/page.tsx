cf14f7b252376e0af9a6009e7724f45b3bcdfc56
Daniel Leonte <leontedaniel37@gmail.com>
2023-04-30 10:58:57 +0300

    refactor handleLinkClick

--- src/app/case-study/[id]/page.tsx
+++ src/app/case-study/[id]/page.tsx
@@ -1,4 +1,15 @@
+"use client";
+import { useEffect } from "react";
+
 const CaseStudyPage = ({ params }: { params: { id: string } }) => {
+  useEffect(() => {
+    console.log("first");
+
+    return () => {
+      console.log("second");
+    };
+  }, []);
+
   return <div>CaseStudyPage {params.id}</div>;
 };
 
