f6f135aa32fb0f780a269b463054755b6711c28f
Daniel Leonte <leontedaniel37@gmail.com>
2023-05-02 07:21:32 +0300

    add response message to contact form

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { AiFillMail } from "react-icons/ai";
 
 export const ContactMe = () => {
   const form = useRef<HTMLFormElement>(null);
+  const [responseStatus, setResponseStatus] = useState(1); // 0 = not sent, 1 = sent, 2 = error
   const SERVICE_ID = "contact_service";
   const TEMPLATE_ID = "contact_form";
 
@@ -23,15 +24,21 @@ export const ContactMe = () => {
       .then(
         (result) => {
           console.log(result.text);
+          if (result.text === "OK") {
+            setResponseStatus(1);
+          } else {
+            setResponseStatus(2);
+          }
         },
         (error) => {
           console.log(error.text);
+          setResponseStatus(2);
         }
       );
   };
 
   return (
-    <div className="flex flex-col justify-between gap-6 my-auto px-10 pb-8">
+    <div className="flex flex-col justify-between gap-6 my-auto px-10 pb-8 relative">
       <h1 className="text-2xl font-bold text-center text-textPrimary">
         Contact Me
       </h1>
@@ -72,7 +79,20 @@ export const ContactMe = () => {
           value="Send"
         />
       </form>
-      <p className="text-center text-sm flex items-center justify-center gap-2 -mb-5"><AiFillMail className="icon" size={18} />lgsoftware99@gmail.com</p>
+      <p className="text-center text-sm flex items-center justify-center gap-2 -mb-3.5 mt-5">
+        <AiFillMail className="icon" size={18} />
+        lgsoftware99@gmail.com
+      </p>
+      {responseStatus === 1 && (
+        <p className="underline underline-offset-4 absolute bottom-[2.99rem] right-0 left-0 text-center text-sm font-medium text-green-500">
+          Message sent successfully!
+        </p>
+      )}
+      {responseStatus === 2 && (
+        <p className="underline underline-offset-4 absolute bottom-[2.99rem] right-0 left-0 text-center text-sm font-medium text-red-500">
+          An error occurred, please try again later.
+        </p>
+      )}
     </div>
   );
 };
