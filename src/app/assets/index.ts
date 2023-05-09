6764c92f274b03d9b51dd5092062b9615e1781e4
Daniel Leonte <leontedaniel37@gmail.com>
2023-05-09 09:17:10 +0300

    fix deployment

--- src/app/assets/index.ts
+++ src/app/assets/index.ts
@@ -1,7 +1,6 @@
 export { default as logo } from './logo.svg';
 export { default as resume } from './resume.png';
 export { default as lineDirection } from './lineDirection.svg';
-export { default as graphic } from './graphic.png';
 export { default as workBackground } from './workBackground.jpg';
 export { default as slackLogo } from './slackLogo.png';
 export { default as mockup } from './mockup.png';
